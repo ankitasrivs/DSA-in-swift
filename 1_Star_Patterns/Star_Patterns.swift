cae38e84e6d18fa4f6abcdf1c14a2a61805ef514
ankitasrivs <66173359+ankitasrivs@users.noreply.github.com>
2025-07-26 18:56:17 +0530

    Update Star_Patterns.swift

--- 1_Star_Patterns/Star_Patterns.swift
+++ 1_Star_Patterns/Star_Patterns.swift
@@ -1,1 +1,20 @@
 
+/*
+* * * * 
+* * * * 
+* * * * 
+* * * * 
+*/
+
+
+func star1(i: Int) {
+    for k in 0..<i {
+        for j in 0..<i {
+            print("*", terminator: " ")
+        }
+        print()
+    }
+}
+star1(i: 4)
+
+
