e91edd36b782e7f69006fad6a92607f4a11080c9
ankitasrivs <66173359+ankitasrivs@users.noreply.github.com>
2025-07-27 17:44:01 +0530

    Update Star_Patterns.swift

--- 1_Star_Patterns/Star_Patterns.swift
+++ 1_Star_Patterns/Star_Patterns.swift
@@ -571,4 +571,50 @@ func star20(i: Int) {
 }
 star20(i: 5)
 
+/*
+2 
+4 3 
+7 6 5 
+11 10 9 8 
+16 15 14 13 12 
+16 15 14 13 12 
+11 10 9 8 
+7 6 5 
+4 3 
+2 
+*/
+
+func star21(i: Int) {
+    var f = 1
+    for k in (1...i){
+       var n = f + k
+        var n1 = n
+        for l in (1...k) {
+            print(n, terminator: " ")
+           n -= 1
+        }
+
+        n += 1
+        f = n1
+        print()
+
+    }
+
+    for k in (1...i).reversed(){
+       var n = f
+
+        for l in (1...k) {
+            print(n, terminator: " ")
+           n -= 1
+        }
+
+
+        f = n
+        print()
+
+    }
+
+}
+star21(i: 5)
+
 
