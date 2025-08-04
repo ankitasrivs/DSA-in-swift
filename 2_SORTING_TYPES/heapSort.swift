a9502533f82c9199958360df9a2685071d53997b
ankitasrivs <66173359+ankitasrivs@users.noreply.github.com>
2025-08-04 09:55:50 +0530

    Update heapSort.swift

--- 2_SORTING_TYPES/heapSort.swift
+++ 2_SORTING_TYPES/heapSort.swift
@@ -1,1 +1,6 @@
+/*
+Heap Sort is a comparison-based sorting algorithm that uses a binary heap data structure. Binary heaps are complete binary trees that satisfy the heap property:
 
+Max-Heap: In a max-heap, for any given node i, the value of i is greater than or equal to the values of its children.
+Min-Heap: In a min-heap, for any given node i, the value of i is less than or equal to the values of its children.
+*/
