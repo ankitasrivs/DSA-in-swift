309ef94741fa013636887d7909573a1d3602fe45
ankitasrivs <66173359+ankitasrivs@users.noreply.github.com>
2025-08-04 14:42:23 +0530

    Update heapSort.swift

--- 2_SORTING_TYPES/heapSort.swift
+++ 2_SORTING_TYPES/heapSort.swift
@@ -46,3 +46,47 @@ func heapSort(_ arr: inout [Int]) {
 var arr = [7,34,2,1]
 heapSort(&arr)
 print(arr)
+
+
+
+/*
+MIN HEAP
+*/
+
+func heapifyMinHeap(_ arr: inout [Int], n: Int, i: Int) {
+    var largest = i
+    let left = 2 * i + 1
+    let right = 2 * i + 2
+
+    if left < n && arr[left] < arr[largest] {
+        largest = left
+    }
+
+    if right < n && arr[right] < arr[largest] {
+        largest = right
+    }
+
+    if largest != i {
+        arr.swapAt(i, largest)
+        heapifyMinHeap(&arr, n: n, i: largest)
+    }
+}
+
+
+func heapSortMinheap(_ arr: inout [Int]) {
+    let n = arr.count
+
+    for i in stride(from: (arr.count - 1)/2, through: 0, by: -1) {
+        heapifyMinHeap(&arr, n: n, i: i)
+    }
+
+    for i in stride(from: arr.count - 1 , through: 0, by: -1) {
+        arr.swapAt(0, i)
+        heapifyMinHeap(&arr, n: i, i: 0)
+    }
+}
+
+
+var arr = [7,34,2,1]
+heapSortMinheap(&arr)
+print(arr)
