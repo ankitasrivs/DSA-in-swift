7a5a3ace27b85f5bdb7dea70d34999f76eba47d8
ankitasrivs <66173359+ankitasrivs@users.noreply.github.com>
2025-08-05 15:29:51 +0530

    Update binarySearch.swift

--- 3_SEARCHING/binarySearch.swift
+++ 3_SEARCHING/binarySearch.swift
@@ -1,1 +1,21 @@
+func binarySearch(arr: [Int], element: Int) -> Int {
+
+  var low = 0
+
+    var high = arr.count - 1
+
+    while low <= high {
+        let mid = (low+high)/2
+        if arr[mid] == element {
+            return mid
+        } else if arr[mid] < element {
+            low = mid + 1
+        } else {
+            high = mid - 1
+        }
+    }
+    return -1
+}
+
+print(binarySearch(arr: [1,2,3,4,21,90], element: 41))
 
