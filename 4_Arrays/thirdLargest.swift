60f07d633edfa7ad383bff5144a77b612c1229fc
ankitasrivs <66173359+ankitasrivs@users.noreply.github.com>
2025-08-06 20:26:22 +0530

    Create thirdLargest.swift

---
+++ 4_Arrays/thirdLargest.swift
@@ -0,0 +1,20 @@
+func thirdLargest(arr: [Int]) {
+    var secondLargest = Int.min
+    var firstLargest = Int.min
+    var thirdLargest = Int.min
+    for i in 0..<arr.count {
+        if arr[i] > firstLargest {
+            thirdLargest = secondLargest
+            secondLargest = firstLargest
+            firstLargest = arr[i]
+        } else if secondLargest < arr[i] && arr[i] != firstLargest {
+            thirdLargest = secondLargest
+            secondLargest = arr[i]
+        } else if thirdLargest < arr[i] && arr[i] != secondLargest && arr[i] != firstLargest {
+            thirdLargest = arr[i]
+        }
+    }
+    print(thirdLargest)
+}
+
+thirdLargest(arr: [1, 14, 2, 16, 10, 20, 16,20])
