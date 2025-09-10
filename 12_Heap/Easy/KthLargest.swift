62c5ef8a5e90428b5ca87bb73c398d664ee4c418
ankitasrivs <66173359+ankitasrivs@users.noreply.github.com>
2025-09-10 10:38:05 +0530

    Create KthLargest.swift

---
+++ 12_Heap/Easy/KthLargest.swift
@@ -0,0 +1,28 @@
+
+// https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+class Solution {
+    func findKthLargest(_ nums: [Int], _ k: Int) -> Int {
+      let minVal = nums.min()!
+      let maxVal = nums.max()!
+      var freq = Array(repeating: 0, count: maxVal - minVal + 1)
+
+      for i in nums {
+        freq[i-minVal] += 1
+      }
+      var rem = k
+
+      for i in stride(from: freq.count - 1, through: 0, by: -1) {
+        rem -= freq[i]
+        if rem <= 0 {
+            return i + minVal
+        }
+      }
+      return -1
+    }
+}
+
+/*
+TIME; O(N)
+Space: O(n)
+*/
