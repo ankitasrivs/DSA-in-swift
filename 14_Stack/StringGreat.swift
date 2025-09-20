741600c24ffd0743e785ade8b4a28c01f191a8d2
ankitasrivs <66173359+ankitasrivs@users.noreply.github.com>
2025-09-20 12:27:05 +0530

    Create StringGreat.swift

---
+++ 14_Stack/StringGreat.swift
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/make-the-string-great/
+
+class Solution {
+    func makeGood(_ s: String) -> String {
+        var stack = [Character]()
+        for i in s {
+            if stack.count > 0 && stack.last!.lowercased() == i.lowercased() && i != stack.last! {
+                stack.removeLast()
+            } else {
+                stack.append(i)
+            }
+        }
+            return String(stack)
+    }
+
+}
+
+
+//Time: O(n)
+//Space: O(n)
