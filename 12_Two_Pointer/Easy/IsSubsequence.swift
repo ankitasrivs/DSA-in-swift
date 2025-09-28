bdfd843f55964138a70b9e517b34fb989a366c8d
ankitasrivs <66173359+ankitasrivs@users.noreply.github.com>
2025-09-28 15:16:56 +0530

    Create IsSubsequence.swift

---
+++ 12_Two_Pointer/Easy/IsSubsequence.swift
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/is-subsequence/
+
+class Solution {
+    func isSubsequence(_ s: String, _ t: String) -> Bool {
+       var i = 0
+       var j = 0
+       let s = Array(s)
+       let t = Array(t)
+
+       while i < s.count && j < t.count {
+        if s[i] == t[j] {
+            i += 1
+            j += 1
+        } else {
+            j  += 1
+        }
+       }
+       return i == s.count
+    }
+}
