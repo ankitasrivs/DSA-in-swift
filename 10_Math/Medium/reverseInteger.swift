5507e75a5710b317858be08b10a3d871d56ab350
ankitasrivs <66173359+ankitasrivs@users.noreply.github.com>
2025-09-30 15:23:23 +0530

    Create reverseInteger.swift

---
+++ 10_Math/Medium/reverseInteger.swift
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/reverse-integer/
+
+class Solution {
+    func reverse(_ x: Int) -> Int {
+        var r = 0, x = x
+        while x != 0 {
+            r = r * 10
+            r = r + (x % 10)
+            x /= 10
+        }
+        return r < Int32.min || r > Int32.max ? 0 : r
+    }
+}
