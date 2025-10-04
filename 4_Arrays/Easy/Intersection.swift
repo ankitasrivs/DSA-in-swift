9cd481d3b0d6d9823212ac7cfdba92ce0f435464
ankitasrivs <66173359+ankitasrivs@users.noreply.github.com>
2025-10-04 17:37:56 +0530

    Create Intersection.swift

---
+++ 4_Arrays/Easy/Intersection.swift
@@ -0,0 +1,9 @@
+//https://leetcode.com/problems/intersection-of-two-arrays/description/
+
+class Solution {
+    func intersection(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+        let set1 = Set(nums1)
+        let set2 = Set(nums2)
+        return Array(set1.intersection(set2))
+    }
+}
