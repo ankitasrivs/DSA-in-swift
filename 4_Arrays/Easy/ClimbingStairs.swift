4f8c3495facc2d060c7e358e79467f792ae8ab76
ankitasrivs <66173359+ankitasrivs@users.noreply.github.com>
2025-10-04 15:45:26 +0530

    Create ClimbingStairs.swift

---
+++ 4_Arrays/Easy/ClimbingStairs.swift
@@ -0,0 +1,19 @@
+
+// https://leetcode.com/problems/climbing-stairs/
+class Solution {
+    func climbStairs(_ n: Int) -> Int {
+      var f1 = 1
+      var f2 = 1
+      var res = 0
+         guard n > 1 else {
+        return 1
+     }
+
+     for _ in 2...n {
+        res = f1 + f2
+        f1 = f2
+        f2 = res
+     }
+     return res
+    }
+}
