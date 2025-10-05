0bbdb2e5e562ad233b2fd76580a0cb4668e4ff7a
ankitasrivs <66173359+ankitasrivs@users.noreply.github.com>
2025-10-05 19:32:07 +0530

    Create RemoveElement.swift

---
+++ 4_Arrays/Easy/RemoveElement.swift
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/remove-element/
+
+class Solution {
+    func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+      var index = 0 
+      for i in nums {
+        if i != val {
+            nums[index] = i
+            index += 1
+        }
+      }
+      return index 
+    }
+}
