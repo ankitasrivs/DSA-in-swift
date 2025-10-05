c1ba4e3f6561ed070f2bd6794bff879985ba55f9
ankitasrivs <66173359+ankitasrivs@users.noreply.github.com>
2025-10-05 14:34:46 +0530

    Create WordPattern.swift

---
+++ 4_Arrays/Easy/WordPattern.swift
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/word-pattern/
+
+class Solution {
+    func wordPattern(_ pattern: String, _ s: String) -> Bool {
+        let pattern = Array(pattern)
+        let s = s.split(separator: " ")
+        var hashMap: [Character: String.SubSequence] = [:]
+
+        if pattern.count != s.count { return false }
+
+        for i in 0..<pattern.count {
+            if hashMap[pattern[i]] == nil {
+                hashMap[pattern[i]] = s[i]
+            } else if hashMap[pattern[i]] != s[i] {
+                return false
+            }
+        }
+
+        return hashMap.keys.count == Set(hashMap.values).count
+    }
+}
