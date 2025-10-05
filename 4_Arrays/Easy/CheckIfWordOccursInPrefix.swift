7a797cde72f5e365f58bfadde2021943a9fa1bf2
ankitasrivs <66173359+ankitasrivs@users.noreply.github.com>
2025-10-05 18:43:13 +0530

    Create CheckIfWordOccursInPrefix.swift

---
+++ 4_Arrays/Easy/CheckIfWordOccursInPrefix.swift
@@ -0,0 +1,15 @@
+
+
+class Solution {
+    func isPrefixOfWord(_ sentence: String, _ searchWord: String) -> Int {
+        let words = sentence.split(separator: " ")
+        
+        for (index, word) in words.enumerated() {
+            if word.hasPrefix(searchWord) {
+                return index + 1 // 1-indexed
+            }
+        }
+        
+        return -1
+    }
+}
