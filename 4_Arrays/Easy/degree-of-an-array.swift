476a28863d14c849a1b89e3e415f9869ff2be27d
ankitasrivs <66173359+ankitasrivs@users.noreply.github.com>
2025-10-15 15:20:14 +0530

    Create degree-of-an-array.swift

---
+++ 4_Arrays/Easy/degree-of-an-array.swift
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/degree-of-an-array/submissions/1802278724/
+
+class Solution {
+    func findShortestSubArray(_ nums: [Int]) -> Int {
+        var freq = [Int: Int]()
+        var first = freq
+        var last = freq
+  
+
+        for i in 0..<nums.count {
+            freq[nums[i], default: 0] += 1
+            last[nums[i]] = i
+            if  first[nums[i]] == nil {
+            first[nums[i]] = i
+            }
+        }
+
+              let degree = freq.values.max()!
+        var minLen = nums.count
+
+        for (k,v) in freq where v == degree {
+            minLen = min(minLen, last[k]! - first[k]! + 1)
+        }
+        return minLen
+    }
+}
