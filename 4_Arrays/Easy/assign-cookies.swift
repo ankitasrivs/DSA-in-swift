b32d22c05403a1a9215f1be2072d6514a3d19c74
ankitasrivs <66173359+ankitasrivs@users.noreply.github.com>
2025-10-15 14:11:28 +0530

    Create assign-cookies.swift

---
+++ 4_Arrays/Easy/assign-cookies.swift
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/assign-cookies/
+
+class Solution {
+    func findContentChildren(_ g: [Int], _ s: [Int]) -> Int {
+      var g = g.sorted()
+      var s = s.sorted()
+      var count = 0
+      var j = 0
+
+      for i in 0..<g.count {
+        while j < s.count && s[j] < g[i] {
+            j += 1
+        }
+        if j < s.count && s[j] >= g[i] {
+            count += 1
+            j += 1
+        }
+      }
+
+      return count
+    }
+}
