7fdc97002709ec3c5e6cf946dd53b70ac89e8f59
ankitasrivs <66173359+ankitasrivs@users.noreply.github.com>
2025-11-05 17:20:23 +0530

    Create merge-intervals.swift

---
+++ 4_Arrays/Medium/merge-intervals.swift
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/merge-intervals/description/
+
+
+class Solution {
+    func merge(_ intervals: [[Int]]) -> [[Int]] {
+      let sorted = intervals.sorted {
+        $0[0] < $1[0]
+      }
+
+      
+             guard let first = sorted.first else { return [[Int]]() }
+
+        var merged = [first]
+for current in sorted {
+            guard let last = merged.last else { continue }
+
+            let prev = last[0]
+            let penult = last[1]
+            
+            let start = current[0], end = current[1]
+            
+            if penult >= start {
+                merged[merged.count - 1] = [prev, max(penult, end)]
+            } else {
+                merged.append(current)
+            }
+        }
+        return merged
+    }
+}
