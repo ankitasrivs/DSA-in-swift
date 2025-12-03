64bcf325a75bd03db99daf89f0720c9f16fdbf04
ankitasrivs <66173359+ankitasrivs@users.noreply.github.com>
2025-12-03 14:56:31 +0530

    Update 1.ReverseString.swift

--- 450-problems/String/1.ReverseString.swift
+++ 450-problems/String/1.ReverseString.swift
@@ -1,1 +1,40 @@
+//
+//  main.swift
+//  DSA-450
+//
+//  Created by Ankita Srivastava on 03/12/25.
+//
+
+import Foundation
+//https://www.geeksforgeeks.org/problems/reverse-a-string/1
+
+func reverseString(str: String) {
+    print(String(str.reversed()))
+}
+
+// Time: O(n)
+//Space: O(n)
+
+reverseString(str: "geeks")
+
+//BETTER
+
+func reverseString1(str: String) {
+    // Array of characters from string
+    var ch = Array(str)
+    var i = 0
+    var j = str.count - 1
+
+    while i < j {
+        ch.swapAt(i, j)
+        i += 1
+        j -= 1
+    }
+    print(String(ch))
+}
+
+// Time: O(n)
+//Space: O(n)
+
+reverseString1(str: "geeks")
 
