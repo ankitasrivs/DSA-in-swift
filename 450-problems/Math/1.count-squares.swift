371e27764b75e5bc30fd069ea284b21328216928
ankitasrivs <66173359+ankitasrivs@users.noreply.github.com>
2025-12-29 14:42:00 +0530

    Update 1.count-squares.swift

--- 450-problems/Math/1.count-squares.swift
+++ 450-problems/Math/1.count-squares.swift
@@ -9,14 +9,23 @@ import Foundation
 //https://www.geeksforgeeks.org/problems/count-squares3649/1
 
 
-func countPerfectSquaresBrute(_ n: Int) -> Int {
-    var count = 0
-    var i = 1
-
-    while i * i < n {
-        count += 1
-        i += 1
+func countPerfectSquaresBinary(_ n: Int) -> Int {
+    if n <= 1 { return 0 }
+    
+    var low = 1
+    var high = n
+    var ans = 0
+    
+    while low <= high {
+        let mid = (low + high) / 2
+        let sq = mid * mid
+        
+        if sq < n {
+            ans = mid
+            low = mid + 1
+        } else {
+            high = mid - 1
+        }
     }
-    return count
+    return ans
 }
-
